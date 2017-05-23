30d4ef413b83108b2d9a74565a3f0af5d423135c
shawn_ian <shawn_ian@outlook.com>
2017-05-23 13:25:42 +0200

    Little Thingie

--- src/main/java/dev/aura/bungeechat/BungeeChat.java
+++ src/main/java/dev/aura/bungeechat/BungeeChat.java
@@ -24,10 +24,7 @@ public class BungeeChat extends Plugin implements BungeeChatApi {
         return null;
     }
     public static Account getUserAccount(ProxiedPlayer player) {
-        for (Account acc : userAccounts) {
-            if (acc.getUniqueId().equals(player.getUniqueId())) return acc;
-        }
-        return null;
+        return getUserAccount(player.getUniqueId());
     }
 
     public static void registerAccount(Account account) { userAccounts.add(account); }
