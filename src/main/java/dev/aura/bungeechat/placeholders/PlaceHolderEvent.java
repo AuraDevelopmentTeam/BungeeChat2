7ef20ca8c81b3d6b90a201a3135d4fa3613bc052
BrainStone <yannick@tedworld.de>
2017-05-23 23:51:39 +0200

    Simplified PlaceHolderEvent

--- src/main/java/dev/aura/bungeechat/placeholders/PlaceHolderEvent.java
+++ src/main/java/dev/aura/bungeechat/placeholders/PlaceHolderEvent.java
@@ -1,22 +1,16 @@
 package dev.aura.bungeechat.placeholders;
 
-import lombok.Getter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Event;
 
+@Data
+@EqualsAndHashCode(callSuper=false)
 public class PlaceHolderEvent extends Event {
-    @Getter
-    private final String message;
-    @Getter
     private final ProxiedPlayer sender;
-    @Getter
     private final ProxiedPlayer target;
-
-    public PlaceHolderEvent(ProxiedPlayer sender, ProxiedPlayer target, String message){
-        this.message = message;
-        this.sender = sender;
-        this.target = target;
-    }
+    private final String message;
 
     public void registerPlaceHolder(String placeholder, String replacement) {
         message.replace(placeholder, replacement);
