427c51b6be82538621732b8164591c588ffef1d1
shawn_ian <shawn_ian@outlook.com>
2017-05-23 18:56:16 +0200

    Added the PlaceHolderEvent (should be in the api but can't import md_5 BungeeCord stuff).

--- src/main/java/dev/aura/bungeechat/placeholders/PlaceHolderEvent.java
+++ src/main/java/dev/aura/bungeechat/placeholders/PlaceHolderEvent.java
@@ -1,2 +1,13 @@
 package dev.aura.bungeechat.placeholders;
 
+import lombok.Getter;
+import net.md_5.bungee.api.plugin.Event;
+
+public class PlaceHolderEvent extends Event {
+    @Getter
+    private final String message;
+
+    public PlaceHolderEvent(String message){
+        this.message = message;
+    }
+}
