74558cddcf82c7437a48b46b44c3985bb7455b51
shawn_ian <shawn_ian@outlook.com>
2017-05-23 11:30:29 +0200

    Updated BungeeChat.java

--- src/main/java/dev/aura/bungeechat/BungeeChat.java
+++ src/main/java/dev/aura/bungeechat/BungeeChat.java
@@ -38,9 +38,9 @@ public class BungeeChat extends Plugin implements BungeeChatApi {
 
     private void loadScreen() {
         Logger.normal(ChatColor.GOLD + "---------------- " + ChatColor.AQUA + "Bungee Chat" + ChatColor.GOLD + " ----------------");
-        Logger.normal(ChatColor.YELLOW + "Authors: " + ChatColor.GREEN + BungeeChatApi.AUTHOR_SHAWN + " & " + BungeeChatApi.AUTHOR_BRAINSTONE);
-        Logger.normal(ChatColor.YELLOW + "Version: " + ChatColor.GREEN + BungeeChatApi.VERSION);
-        Logger.normal(ChatColor.YELLOW + "Build: " + ChatColor.GREEN + BungeeChatApi.BUILD);
+        Logger.normal(ChatColor.YELLOW + "Authors: " + ChatColor.GREEN + AUTHOR_SHAWN + " & " + AUTHOR_BRAINSTONE);
+        Logger.normal(ChatColor.YELLOW + "Version: " + ChatColor.GREEN + VERSION);
+        Logger.normal(ChatColor.YELLOW + "Build: " + ChatColor.GREEN + BUILD);
         //TODO: Loaded Modules Displayed.
         if (isLatestVersion()) {
             Logger.normal(ChatColor.YELLOW + "There is an update avalible. You can download version " + ChatColor.GREEN
