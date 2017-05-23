c5aa90a6d41980c1c5c94b81f54eb38dc315f845
shawn_ian <shawn_ian@outlook.com>
2017-05-23 18:34:37 +0200

    Permissions Updated.

--- src/main/java/dev/aura/bungeechat/permissions/Permission.java
+++ src/main/java/dev/aura/bungeechat/permissions/Permission.java
@@ -6,11 +6,12 @@ public enum Permission {
 
     USE_COLORED_CHAT("bungeechat.chat.colors"),
 
-    BYPASS_ALL("bungeechat.chat.bypassall"),
+    BYPASS_ALL("bungeechat.chat.*"),
     BYPASS_ANTI_ADVERTISEMENT("bungeechat.chat.bypassantiadvertisement"),
     BYPASS_ANTI_SPAM("bungeechat.chat.bypassantispam"),
     BYPASS_ANTI_SWEAR("bungeechat.chat.bypassantiswear"),
 
+    COMMAND_ALL("bungeechat.command.*"),
     COMMAND_ALERT("bungeechat.command.alert"),
     COMMAND_GLOBAL("bungeechat.command.global"),
     COMMAND_STAFFCHAT("bungeechat.command.staffchat"),
