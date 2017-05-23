33198f873379833e75885aaeb7549060c740cebe
shawn_ian <shawn_ian@outlook.com>
2017-05-23 10:44:19 +0200

    Added the Accounts Array.

--- src/main/java/dev/aura/bungeechat/BungeeChat.java
+++ src/main/java/dev/aura/bungeechat/BungeeChat.java
@@ -1,8 +1,30 @@
 package dev.aura.bungeechat;
 
 import dev.aura.bungeechat.api.BungeeChatApi;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
 
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class BungeeChat extends Plugin implements BungeeChatApi {
+
+    private static CopyOnWriteArrayList<Account> userAccounts = new CopyOnWriteArrayList<>();
+
+    public static Account getUserAccount(UUID uuid) {
+        for (Account acc : userAccounts) {
+            if (acc.getUniqueId().equals(uuid)) return acc;
+        }
+        return null;
+    }
+    public static Account getUserAccount(ProxiedPlayer player) {
+        for (Account acc : userAccounts) {
+            if (acc.getUniqueId().equals(player.getUniqueId())) return acc;
+        }
+        return null;
+    }
+
+    public static void registerAccount(Account account) { userAccounts.add(account); }
+    public static void unregisterAccount(Account account) { userAccounts.remove(account); }
 
 }
