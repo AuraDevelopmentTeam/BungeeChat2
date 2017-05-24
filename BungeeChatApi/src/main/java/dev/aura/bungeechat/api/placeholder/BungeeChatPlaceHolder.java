5ee344f78542029b2c7bd1ac78d1b57ca858d174
BrainStone <yannick@tedworld.de>
2017-05-24 20:09:33 +0200

    Added BungeChatPlaceHolder interface
    - Default implementation is PlaceHolder

---
+++ BungeeChatApi/src/main/java/dev/aura/bungeechat/api/placeholder/BungeeChatPlaceHolder.java
@@ -0,0 +1,7 @@
+package dev.aura.bungeechat.api.placeholder;
+
+public interface BungeeChatPlaceHolder {
+	public boolean isContextApplicable(BungeeChatContext context);
+	public String apply(String message, BungeeChatContext context);
+	public String getName();
+}
--- BungeeChatApi/src/main/java/dev/aura/bungeechat/api/placeholder/PlaceHolderManager.java
+++ BungeeChatApi/src/main/java/dev/aura/bungeechat/api/placeholder/PlaceHolderManager.java
@@ -9,26 +9,26 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class PlaceHolderManager {
-	private static final List<Placeholder> placeholders = new LinkedList<Placeholder>();
+	private static final List<BungeeChatPlaceHolder> placeholders = new LinkedList<BungeeChatPlaceHolder>();
 
-	public static Stream<Placeholder> getPlaceholderStream() {
+	public static Stream<BungeeChatPlaceHolder> getPlaceholderStream() {
 		return placeholders.stream();
 	}
 
-	public static Stream<Placeholder> getApplicableStream(BungeeChatContext context) {
+	public static Stream<BungeeChatPlaceHolder> getApplicableStream(BungeeChatContext context) {
 		return getPlaceholderStream().filter(placeholder -> placeholder.isContextApplicable(context));
 	}
 
 	public static String processMessage(String message, BungeeChatContext context) {
-		for (Placeholder placeholder : getApplicableStream(context).collect(Collectors.toSet()))
+		for (BungeeChatPlaceHolder placeholder : getApplicableStream(context).collect(Collectors.toSet()))
 			message = placeholder.apply(message, context);
 
 		return message;
 	}
 
-	public static void registerPlaceholder(Placeholder placeholder) {
+	public static void registerPlaceholder(BungeeChatPlaceHolder placeholder) {
 		if(placeholders.contains(placeholder))
-			throw new IllegalStateException("Placeholder " + placeholder.getPlaceholder() + " has already been registered!");
+			throw new IllegalStateException("Placeholder " + placeholder.toString() + " has already been registered!");
 		
 		placeholders.add(placeholder);
 	}
