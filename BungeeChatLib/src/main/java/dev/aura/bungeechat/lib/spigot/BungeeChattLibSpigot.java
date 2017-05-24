35adbe912c9769643fa57a3d526cd43bf53a231e
BrainStone <yannick@tedworld.de>
2017-05-24 20:10:32 +0200

    More cleanup

--- BungeeChatLib/src/main/java/dev/aura/bungeechat/lib/spigot/BungeeChattLibSpigot.java
+++ BungeeChatLib/src/main/java/dev/aura/bungeechat/lib/spigot/BungeeChattLibSpigot.java
@@ -8,14 +8,14 @@ import dev.aura.bungeechat.api.enums.ServerType;
 import dev.aura.bungeechat.api.interfaces.BungeeChatAccount;
 
 public class BungeeChattLibSpigot extends JavaPlugin implements BungeeChatApi {
-	@Override
-	public ServerType getServerType() {
-		return ServerType.BUKKIT;
-	}
+    @Override
+    public ServerType getServerType() {
+        return ServerType.BUKKIT;
+    }
 
-	@Override
-	public boolean hasPermission(BungeeChatAccount account, Permission permission) {
-		// TODO: Get via channel messages!
-		throw new UnsupportedOperationException("Not implemented!");
-	}
+    @Override
+    public boolean hasPermission(BungeeChatAccount account, Permission permission) {
+        // TODO: Get via channel messages!
+        throw new UnsupportedOperationException("Not implemented!");
+    }
 }
--- BungeeChatLib/src/main/java/dev/aura/bungeechat/lib/sponge/BungeeChatLibSponge.java
+++ BungeeChatLib/src/main/java/dev/aura/bungeechat/lib/sponge/BungeeChatLibSponge.java
@@ -8,16 +8,16 @@ import dev.aura.bungeechat.api.enums.ServerType;
 import dev.aura.bungeechat.api.interfaces.BungeeChatAccount;
 
 @Plugin(id = BungeeChatApi.ID_LIB, name = BungeeChatApi.NAME_LIB, version = BungeeChatApi.VERSION, description = BungeeChatApi.DESCRIPTION_LIB, url = BungeeChatApi.URL, authors = {
-		BungeeChatApi.AUTHOR_SHAWN, BungeeChatApi.AUTHOR_BRAINSTONE })
+        BungeeChatApi.AUTHOR_SHAWN, BungeeChatApi.AUTHOR_BRAINSTONE })
 public class BungeeChatLibSponge implements BungeeChatApi {
-	@Override
-	public ServerType getServerType() {
-		return ServerType.SPONGE;
-	}
-	
-	@Override
-	public boolean hasPermission(BungeeChatAccount account, Permission permission) {
-		// TODO: Get via channel messages!
-		throw new UnsupportedOperationException("Not implemented!");
-	}
+    @Override
+    public ServerType getServerType() {
+        return ServerType.SPONGE;
+    }
+
+    @Override
+    public boolean hasPermission(BungeeChatAccount account, Permission permission) {
+        // TODO: Get via channel messages!
+        throw new UnsupportedOperationException("Not implemented!");
+    }
 }
