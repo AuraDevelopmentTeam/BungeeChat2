01f9ce41c82d8ca187164f169556b6076d27e208
BrainStone <yannick@tedworld.de>
2017-05-24 19:37:38 +0200

    Using nicer syntax

--- BungeeChatApi/src/main/java/dev/aura/bungeechat/api/placeholder/BungeeChatContext.java
+++ BungeeChatApi/src/main/java/dev/aura/bungeechat/api/placeholder/BungeeChatContext.java
@@ -11,23 +11,23 @@ public class BungeeChatContext {
 	public static final Predicate<BungeeChatContext> HAS_SENDER = context -> context.hasSender();
 	public static final Predicate<BungeeChatContext> HAS_TARGET = context -> context.hasTarget();
 
-	public static final Predicate<BungeeChatContext> HAS_NO_PLAYER = context -> !context.hasPlayer();
-	public static final Predicate<BungeeChatContext> HAS_NO_SENDER = context -> !context.hasSender();
-	public static final Predicate<BungeeChatContext> HAS_NO_TARGET = context -> !context.hasTarget();
-	
+	public static final Predicate<BungeeChatContext> HAS_NO_PLAYER = HAS_PLAYER.negate();
+	public static final Predicate<BungeeChatContext> HAS_NO_SENDER = HAS_SENDER.negate();
+	public static final Predicate<BungeeChatContext> HAS_NO_TARGET = HAS_TARGET.negate();
+
 	@Getter
 	protected final Optional<BungeeChatAccount> player;
 	@Getter
 	protected final Optional<BungeeChatAccount> sender;
 	@Getter
 	protected final Optional<BungeeChatAccount> target;
-	
+
 	public BungeeChatContext(BungeeChatAccount player) {
 		this.player = Optional.of(player);
 		sender = Optional.empty();
 		target = Optional.empty();
 	}
-	
+
 	public BungeeChatContext(BungeeChatAccount sender, BungeeChatAccount target) {
 		player = Optional.empty();
 		this.sender = Optional.of(sender);
