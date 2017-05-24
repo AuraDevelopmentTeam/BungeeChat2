8b768fd9c41e007bdd4850bb4a5f2ab9a9a37c60
BrainStone <yannick@tedworld.de>
2017-05-24 23:35:21 +0200

    Optimized Config

--- src/main/java/dev/aura/bungeechat/config/Config.java
+++ src/main/java/dev/aura/bungeechat/config/Config.java
@@ -2,7 +2,6 @@ package dev.aura.bungeechat.config;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 
 import dev.aura.bungeechat.Logger;
@@ -13,7 +12,6 @@ import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
 public class Config {
-
     private static Configuration configuration;
 
     public static void load() {
@@ -21,14 +19,13 @@ public class Config {
         if (!folder.exists()) {
             folder.mkdir();
         }
-        File cfile = new File(ProxyServer.getInstance().getPluginsFolder() + "/BungeeChat/config.yml");
+        File cfile = new File(folder, "config.yml");
         try {
             if (!cfile.exists()) {
                 Files.copy(ProxyServer.getInstance().getPluginManager().getPlugin("BungeeChat")
-                        .getResourceAsStream("config.yml"), cfile.toPath(), new CopyOption[0]);
+                        .getResourceAsStream("config.yml"), cfile.toPath());
             }
-            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class)
-                    .load(new File(ProxyServer.getInstance().getPluginsFolder() + "/BungeeChat/config.yml"));
+            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(cfile);
         } catch (Exception e) {
             e.printStackTrace();
             Logger.error("There is an error with creating or loading the conifg file!");
