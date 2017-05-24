3186441b27e6f340e46d89c1bc0e71b9394dafaa
BrainStone <yannick@tedworld.de>
2017-05-24 14:13:38 +0200

    Improved Permission stuff

--- src/main/java/dev/aura/bungeechat/permission/Permission.java
+++ src/main/java/dev/aura/bungeechat/permission/Permission.java
@@ -1,31 +1,28 @@
 package dev.aura.bungeechat.permission;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum Permission {
+	BUNGEECHAT_RELOAD("bungeechat.admin.reload"),
 
-    BUNGEECHAT_RELOAD("bungeechat.admin.reload"),
+	USE_COLORED_CHAT("bungeechat.chat.colors"),
 
-    USE_COLORED_CHAT("bungeechat.chat.colors"),
+	BYPASS_ALL("bungeechat.chat.*"),
+	BYPASS_ANTI_ADVERTISEMENT("bungeechat.chat.bypassantiadvertisement"),
+	BYPASS_ANTI_SPAM("bungeechat.chat.bypassantispam"),
+	BYPASS_ANTI_SWEAR("bungeechat.chat.bypassantiswear"),
 
-    BYPASS_ALL("bungeechat.chat.*"),
-    BYPASS_ANTI_ADVERTISEMENT("bungeechat.chat.bypassantiadvertisement"),
-    BYPASS_ANTI_SPAM("bungeechat.chat.bypassantispam"),
-    BYPASS_ANTI_SWEAR("bungeechat.chat.bypassantiswear"),
+	COMMAND_ALL("bungeechat.command.*"),
+	COMMAND_ALERT("bungeechat.command.alert"),
+	COMMAND_GLOBAL("bungeechat.command.global"),
+	COMMAND_STAFFCHAT("bungeechat.command.staffchat"),
+	COMMAND_HELPOP("bungeechat.command.helpop"),
+	COMMAND_IGNORE("bungeechat.command.ignore"),
+	COMMAND_MESSAGE("bungeechat.command.msg"),
+	COMMAND_TOGGLE_MESSAGE("bungeechat.command.toggle");
 
-    COMMAND_ALL("bungeechat.command.*"),
-    COMMAND_ALERT("bungeechat.command.alert"),
-    COMMAND_GLOBAL("bungeechat.command.global"),
-    COMMAND_STAFFCHAT("bungeechat.command.staffchat"),
-    COMMAND_HELPOP("bungeechat.command.helpop"),
-    COMMAND_IGNORE("bungeechat.command.ignore"),
-    COMMAND_MESSAGE("bungeechat.command.msg"),
-    COMMAND_TOGGLE_MESSAGE("bungeechat.command.toggle");
-
-    Permission(String stringedPermission){
-        this.stringedPermission = stringedPermission;
-    }
-
-    private String stringedPermission;
-
-    public String getStringedPermission() { return this.stringedPermission; }
-
+	@Getter
+	private final String stringedPermission;
 }
--- src/main/java/dev/aura/bungeechat/permission/PermissionManager.java
+++ src/main/java/dev/aura/bungeechat/permission/PermissionManager.java
@@ -1,22 +1,36 @@
 package dev.aura.bungeechat.permission;
 
+import dev.aura.bungeechat.api.interfaces.BungeeChatAccount;
+import lombok.experimental.UtilityClass;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+@UtilityClass
 public class PermissionManager {
+	public static boolean hasPermission(ProxiedPlayer player, Permission permission) {
+		if (player.hasPermission(permission.getStringedPermission()))
+			return true;
+		else {
+			if (permission.getStringedPermission().startsWith("bungeechat.chat.")
+					&& player.hasPermission(Permission.BYPASS_ALL.getStringedPermission()))
+				return true;
+			else if (permission.getStringedPermission().startsWith("bungeechat.command.")
+					&& player.hasPermission(Permission.COMMAND_ALL.getStringedPermission()))
+				return true;
+			else
+				return false;
+		}
+	}
 
-    public static boolean hasPermission(ProxiedPlayer player, Permission permission){
-        if (player.hasPermission(permission.getStringedPermission())) return true;
-        else {
-            if (permission.getStringedPermission().startsWith("bungeechat.chat.") && player.hasPermission(Permission.BYPASS_ALL.getStringedPermission())) return true;
-            else if (permission.getStringedPermission().startsWith("bungeechat.command.") && player.hasPermission(Permission.COMMAND_ALL.getStringedPermission())) return true;
-            else return false;
-        }
-    }
+	public static boolean hasPermission(CommandSender sender, Permission permission) {
+		if (sender instanceof ProxiedPlayer)
+			return hasPermission((ProxiedPlayer) sender, permission);
+		else
+			return true;
+	}
 
-    public static boolean hasPermission(CommandSender sender, Permission permission){
-        if (sender instanceof ProxiedPlayer) return hasPermission((ProxiedPlayer) sender, permission);
-        else return true;
-    }
-
+	public static boolean hasPermission(BungeeChatAccount account, Permission permission) {
+		return hasPermission(ProxyServer.getInstance().getPlayer(account.getUniqueId()), permission);
+	}
 }
