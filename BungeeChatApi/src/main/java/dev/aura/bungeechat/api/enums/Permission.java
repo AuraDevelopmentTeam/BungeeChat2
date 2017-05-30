fbead0abb8e17f31f5ae22877ee428be9d8e0298
BrainStone <yannick@tedworld.de>
2017-05-30 18:29:23 +0200

    Made Permission Enum smaller

--- BungeeChatApi/src/main/java/dev/aura/bungeechat/api/enums/Permission.java
+++ BungeeChatApi/src/main/java/dev/aura/bungeechat/api/enums/Permission.java
@@ -1,36 +1,38 @@
 package dev.aura.bungeechat.api.enums;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public enum Permission {
-    BUNGEECHAT_RELOAD("bungeechat.admin.reload"),
+    BUNGEECHAT_RELOAD("admin.reload"),
 
-    USE_COLORED_CHAT("bungeechat.chat.colors"),
+    USE_COLORED_CHAT("chat.colors"),
 
-    BYPASS_ALL("bungeechat.chat.*"),
-    BYPASS_ANTI_ADVERTISEMENT("bungeechat.chat.bypassantiadvertisement"),
-    BYPASS_ANTI_SPAM("bungeechat.chat.bypassantispam"),
-    BYPASS_ANTI_SWEAR("bungeechat.chat.bypassantiswear"),
+    BYPASS_ALL("chat.*"),
+    BYPASS_ANTI_ADVERTISEMENT("chat.bypassantiadvertisement"),
+    BYPASS_ANTI_SPAM("chat.bypassantispam"),
+    BYPASS_ANTI_SWEAR("chat.bypassantiswear"),
 
-    COMMAND_ALL("bungeechat.command.*"),
-    COMMAND_ALERT("bungeechat.command.alert"),
-    COMMAND_GLOBAL("bungeechat.command.global"),
-    COMMAND_GLOBAL_TOGGLE("bungeechat.command.global.toggle"),
-    COMMAND_STAFFCHAT("bungeechat.command.staffchat"),
-    COMMAND_STAFFCHAT_VIEW("bungeechat.command.staffchat.view"),
-    COMMAND_HELPOP("bungeechat.command.helpop"),
-    COMMAND_HELPOP_VIEW("bungeechat.command.helpop.view"),
-    COMMAND_IGNORE("bungeechat.command.ignore"),
-    COMMAND_IGNORE_BYPASS("bungeechat.command.ignore.bypass"),
-    COMMAND_MESSAGE("bungeechat.command.msg"),
-    COMMAND_TOGGLE_MESSAGE("bungeechat.command.toggle"),
-    COMMAND_TOGGLE_MESSAGE_BYPASS("bungeechat.command.toggle.bypass"),
-    COMMAND_VANISH("bungeechat.command.vanish"),
-    COMMAND_VANISH_SEE("bungeechat.command.vanish.see"),
-    COMMAND_SOCIALSPY("bungeechat.command.socialspy");
+    COMMAND_ALL("command.*"),
+    COMMAND_ALERT("command.alert"),
+    COMMAND_GLOBAL("command.global"),
+    COMMAND_GLOBAL_TOGGLE("command.global.toggle"),
+    COMMAND_STAFFCHAT("command.staffchat"),
+    COMMAND_STAFFCHAT_VIEW("command.staffchat.view"),
+    COMMAND_HELPOP("command.helpop"),
+    COMMAND_HELPOP_VIEW("command.helpop.view"),
+    COMMAND_IGNORE("command.ignore"),
+    COMMAND_IGNORE_BYPASS("command.ignore.bypass"),
+    COMMAND_MESSAGE("command.msg"),
+    COMMAND_TOGGLE_MESSAGE("command.toggle"),
+    COMMAND_TOGGLE_MESSAGE_BYPASS("command.toggle.bypass"),
+    COMMAND_VANISH("command.vanish"),
+    COMMAND_VANISH_SEE("command.vanish.see"),
+    COMMAND_SOCIALSPY("command.socialspy");
 
     @Getter
     private final String stringedPermission;
+
+    private Permission(String stringedPermission) {
+        this.stringedPermission = "bungeechat." + stringedPermission;
+    }
 }
