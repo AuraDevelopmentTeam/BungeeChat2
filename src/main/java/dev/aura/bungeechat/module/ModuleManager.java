868421a1ab678f1fa2000476c7d2ee0d8322a715
BrainStone <yannick@tedworld.de>
2017-06-01 12:17:45 +0200

    Sorted modules alphabetically

--- src/main/java/dev/aura/bungeechat/module/ModuleManager.java
+++ src/main/java/dev/aura/bungeechat/module/ModuleManager.java
@@ -14,14 +14,14 @@ public class ModuleManager {
     public static final AntiSwearModule ANTI_SWEAR_MODULE = new AntiSwearModule();
     public static final GlobalChatModule GLOBAL_CHAT_MODULE = new GlobalChatModule();
     public static final HelpOpModule HELP_OP_MODULE = new HelpOpModule();
+    public static final JoinMessageModule JOIN_MESSAGE_MODULE = new JoinMessageModule();
+    public static final LeaveMessageModule LEAVE_MESSAGE_MODULE = new LeaveMessageModule();
+    public static final LocalChatModule LOCAL_CHAT_MODULE = new LocalChatModule();
     public static final MessengerModule MESSENGER_MODULE = new MessengerModule();
     public static final SocialSpyModule SOCIAL_SPY_MODULE = new SocialSpyModule();
-    public static final VanisherModule VANISHER_MODULE = new VanisherModule();
     public static final StaffChatModule STAFF_CHAT_MODULE = new StaffChatModule();
-    public static final LocalChatModule LOCAL_CHAT_MODULE = new LocalChatModule();
-    public static final LeaveMessageModule LEAVE_MESSAGE_MODULE = new LeaveMessageModule();
-    public static final JoinMessageModule JOIN_MESSAGE_MODULE = new JoinMessageModule();
     public static final TabCompletionModule TAB_COMPLETION_MODULE = new TabCompletionModule();
+    public static final VanisherModule VANISHER_MODULE = new VanisherModule();
 
     private static List<Module> activeModules = null;
     private static String MODULE_CONCATENATOR = ChatColor.WHITE + ", " + ChatColor.GREEN;
