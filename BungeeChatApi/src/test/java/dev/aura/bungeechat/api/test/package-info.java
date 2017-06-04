b4751285d34343988788d6cf7300006b3a2d78ab
BrainStone <yannick@tedworld.de>
2017-06-04 03:02:19 +0200

    Added test sourceSets

---
+++ BungeeChatApi/src/test/java/dev/aura/bungeechat/api/test/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package is used for tests of the API.<br>
+ * Please use separate files for each section of tests.
+ */
+package dev.aura.bungeechat.api.test;
---
+++ src/test/java/dev/aura/bungeechat/test/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package is used for tests of the plugin.<br>
+ * Please use separate files for each section of tests.
+ */
+package dev.aura.bungeechat.test;
