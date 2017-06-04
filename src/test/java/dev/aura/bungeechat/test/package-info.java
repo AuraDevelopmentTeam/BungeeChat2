61ea7439618ab3758480e27b218b613935350c57
BrainStone <yannick@tedworld.de>
2017-06-04 04:21:13 +0200

    Expanded package-info for the tests

--- src/test/java/dev/aura/bungeechat/test/package-info.java
+++ src/test/java/dev/aura/bungeechat/test/package-info.java
@@ -1,5 +1,8 @@
 /**
  * This package is used for tests of the plugin.<br>
- * Please use separate files for each section of tests.
+ * Please use separate files for each section of tests.<br>
+ * <br>
+ * Please try to create a class for each class you test and name it
+ * (ClassName)Test. Exceptions may apply.
  */
 package dev.aura.bungeechat.test;
