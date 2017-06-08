ea3bf4fcdbf1666bdd619478c702580480b7126c
shawn_ian <shawn_ian@outlook.com>
2017-06-08 16:16:30 +0200

    Unmute Command.

--- src/main/java/dev/aura/bungeechat/command/UnmuteCommand.java
+++ src/main/java/dev/aura/bungeechat/command/UnmuteCommand.java
@@ -1,10 +1,14 @@
 package dev.aura.bungeechat.command;
 
+import dev.aura.bungeechat.account.BungeecordAccountManager;
+import dev.aura.bungeechat.api.account.BungeeChatAccount;
 import dev.aura.bungeechat.api.enums.Permission;
 import dev.aura.bungeechat.message.Message;
 import dev.aura.bungeechat.module.MutingModule;
 import dev.aura.bungeechat.permission.PermissionManager;
 import net.md_5.bungee.api.CommandSender;
+
+import java.util.Optional;
 
 public class UnmuteCommand extends BaseCommand {
     public UnmuteCommand(MutingModule mutingModule) {
@@ -16,9 +20,23 @@ public class UnmuteCommand extends BaseCommand {
     public void execute(CommandSender sender, String[] args) {
         if (PermissionManager.hasPermission(sender, Permission.COMMAND_UNMUTE)) {
             if (args.length < 1) {
-                sender.sendMessage(Message.INCORRECT_USAGE.get(sender, "/mute <player>"));
+                sender.sendMessage(Message.INCORRECT_USAGE.get(sender, "/unmute <player>"));
             } else {
-                //TODO UnMute Stuff.
+                Optional<BungeeChatAccount> targetAccount = BungeecordAccountManager.getAccount(sender);
+
+                if (!targetAccount.isPresent()) {
+                    sender.sendMessage(Message.PLAYER_NOT_FOUND.get());
+                    return;
+                }
+
+                if (!targetAccount.get().isMuted()) {
+                    sender.sendMessage(Message.UNMUTE_NOT_MUTED.get());
+                    return;
+                }
+
+                targetAccount.get().unmute();
+                sender.sendMessage(Message.UNMUTE.get(sender));
+
             }
         }
     }
