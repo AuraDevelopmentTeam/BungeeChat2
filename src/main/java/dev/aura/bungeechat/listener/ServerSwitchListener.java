f026221ee8ad1c4406900a63dd047ba9c96cecf0
BrainStone <yannick@tedworld.de>
2017-06-08 19:12:10 +0200

    Server switch message doesn't get displayed when player joins

--- src/main/java/dev/aura/bungeechat/listener/ServerSwitchListener.java
+++ src/main/java/dev/aura/bungeechat/listener/ServerSwitchListener.java
@@ -1,21 +1,40 @@
 package dev.aura.bungeechat.listener;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
 import dev.aura.bungeechat.api.enums.Permission;
 import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.permission.PermissionManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
 public class ServerSwitchListener implements Listener {
-    @EventHandler(priority = EventPriority.HIGH)
+    private final List<UUID> skipPlayer = new LinkedList<>();
+
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerServerSwitch(ServerSwitchEvent e) {
         ProxiedPlayer player = e.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        if (skipPlayer.contains(uuid)) {
+            skipPlayer.remove(uuid);
+
+            return;
+        }
 
         if (PermissionManager.hasPermission(player, Permission.MESSAGE_SWITCH)) {
             MessagesService.sendSwitchMessage(player);
         }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PostLoginEvent e) {
+        skipPlayer.add(e.getPlayer().getUniqueId());
     }
 }
