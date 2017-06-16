e2781af556c550599a94016fe5580becb808fa8e
BrainStone <yannick@tedworld.de>
2017-06-16 16:36:14 +0200

    Added test to RegexUtilTest
    - There should be more!

---
+++ BungeeChatApi/src/test/java/dev/aura/bungeechat/api/test/util/RegexUtilTest.java
@@ -0,0 +1,39 @@
+package dev.aura.bungeechat.api.test.util;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+import dev.aura.bungeechat.api.utils.RegexUtil;
+
+public class RegexUtilTest {
+    @Test
+    public void leetSpeakPatterns() {
+        for (RegexUtil.LeetSpeakPattern leet : RegexUtil.LEET_PATTERNS.values()) {
+            String letter = leet.getLetter();
+            Pattern pattern = Pattern.compile(leet.getPattern());
+
+            for (String replacement : leet.getLeetAlternatives()) {
+                assertTrue("Replacement \"" + replacement + "\" doesn't match as a alternative for " + letter + "!",
+                        pattern.matcher(replacement).matches());
+            }
+        }
+    }
+    
+    @Test
+    public void leetSpeakWildcard() {
+        for (RegexUtil.LeetSpeakPattern leet : RegexUtil.LEET_PATTERNS.values()) {
+            String letter = leet.getLetter();
+            Pattern pattern = RegexUtil.parseWildcardToPattern(leet.getLetter(), Pattern.CASE_INSENSITIVE, false, true, false, false);
+
+            for (String replacement : leet.getLeetAlternatives()) {
+                assertTrue("Replacement \"" + replacement + "\" doesn't match as a alternative for " + letter + "!",
+                        pattern.matcher(replacement).matches());
+            }
+        }
+    }
+    
+    // TODO: More tests!
+}
