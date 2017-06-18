d1050109c763bb7f8c2125ea81d7423f099297d3
BrainStone <yannick@tedworld.de>
2017-06-18 04:07:23 +0200

    JoinMessage now has access to the server
    - Also it won't be shown when the player gets kicked immedieatly

--- src/main/java/dev/aura/bungeechat/listener/JoinMessageListener.java
+++ src/main/java/dev/aura/bungeechat/listener/JoinMessageListener.java
@@ -1,21 +1,39 @@
 package dev.aura.bungeechat.listener;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
 import dev.aura.bungeechat.api.enums.Permission;
 import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.permission.PermissionManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
 public class JoinMessageListener implements Listener {
+    private final List<UUID> joinedPlayers = new LinkedList<>();
+
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerJoin(PostLoginEvent e) {
+    public void onPlayerServerSwitch(ServerSwitchEvent e) {
         ProxiedPlayer player = e.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        if (joinedPlayers.contains(uuid))
+            return;
+
+        joinedPlayers.add(uuid);
 
         if (PermissionManager.hasPermission(player, Permission.MESSAGE_JOIN)) {
             MessagesService.sendJoinMessage(player);
         }
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerDisconnectEvent e) {
+        joinedPlayers.remove(e.getPlayer().getUniqueId());
     }
 }
