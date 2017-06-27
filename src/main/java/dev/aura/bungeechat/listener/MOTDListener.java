3f20c2952d35cda905687950ac17be1a0f3156b4
shawn_ian <shawn_ian@outlook.com>
2017-06-27 09:41:08 +0200

    Fixed Context in MOTD

--- src/main/java/dev/aura/bungeechat/listener/MOTDListener.java
+++ src/main/java/dev/aura/bungeechat/listener/MOTDListener.java
@@ -2,7 +2,9 @@ package dev.aura.bungeechat.listener;
 
 import dev.aura.bungeechat.account.BungeecordAccountManager;
 import dev.aura.bungeechat.api.account.BungeeChatAccount;
+import dev.aura.bungeechat.api.placeholder.BungeeChatContext;
 import dev.aura.bungeechat.message.MessagesService;
+import dev.aura.bungeechat.message.PlaceHolderUtil;
 import dev.aura.bungeechat.module.BungeecordModuleManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
@@ -19,7 +21,7 @@ public class MOTDListener implements Listener {
         ProxiedPlayer player = e.getPlayer();
         BungeeChatAccount bungeeChatAccount = BungeecordAccountManager.getAccount(player).get();
         ArrayList<String> motd = (ArrayList<String>) BungeecordModuleManager.MOTD_MODULE.getModuleSection().getStringList("message");
-        motd.stream().forEach(message -> MessagesService.sendMessage(player, message));
+        motd.stream().forEach(message -> MessagesService.sendMessage(player, PlaceHolderUtil.formatMessage(message, new BungeeChatContext(bungeeChatAccount))));
     }
 
 }
