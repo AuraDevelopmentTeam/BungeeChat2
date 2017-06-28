2cfa88bb91df2fd1c292ca3e8052dbc9652e015d
shawn_ian <shawn_ian@outlook.com>
2017-06-28 13:57:57 +0200

    Cleanup

--- src/main/java/dev/aura/bungeechat/task/AutomaticBroadcastTask.java
+++ src/main/java/dev/aura/bungeechat/task/AutomaticBroadcastTask.java
@@ -2,7 +2,6 @@ package dev.aura.bungeechat.task;
 
 import dev.aura.bungeechat.BungeeChat;
 import dev.aura.bungeechat.api.placeholder.BungeeChatContext;
-import dev.aura.bungeechat.chatlog.ChatLoggingManager;
 import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.message.PlaceHolderUtil;
 import dev.aura.bungeechat.module.BungeecordModuleManager;
