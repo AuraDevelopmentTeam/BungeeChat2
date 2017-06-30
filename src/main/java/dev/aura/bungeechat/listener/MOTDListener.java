b59011b2e9461a6c186eb6c45929d67e98f93f9e
shawn_ian <shawn_ian@outlook.com>
2017-06-30 23:05:30 +0200

    Added Permission for MOTD.

--- src/main/java/dev/aura/bungeechat/listener/MOTDListener.java
+++ src/main/java/dev/aura/bungeechat/listener/MOTDListener.java
@@ -2,10 +2,12 @@ package dev.aura.bungeechat.listener;
 
 import dev.aura.bungeechat.account.BungeecordAccountManager;
 import dev.aura.bungeechat.api.account.BungeeChatAccount;
+import dev.aura.bungeechat.api.enums.Permission;
 import dev.aura.bungeechat.api.placeholder.BungeeChatContext;
 import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.message.PlaceHolderUtil;
 import dev.aura.bungeechat.module.BungeecordModuleManager;
+import dev.aura.bungeechat.permission.PermissionManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -19,9 +21,12 @@ public class MOTDListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PostLoginEvent e) {
         ProxiedPlayer player = e.getPlayer();
-        BungeeChatAccount bungeeChatAccount = BungeecordAccountManager.getAccount(player).get();
-        ArrayList<String> motd = (ArrayList<String>) BungeecordModuleManager.MOTD_MODULE.getModuleSection().getStringList("message");
-        motd.stream().forEach(message -> MessagesService.sendMessage(player, PlaceHolderUtil.formatMessage(message, new BungeeChatContext(bungeeChatAccount))));
+        if (PermissionManager.hasPermission(player, Permission.MESSAGE_MOTD)) {
+            BungeeChatAccount bungeeChatAccount = BungeecordAccountManager.getAccount(player).get();
+            ArrayList<String> motd = (ArrayList<String>) BungeecordModuleManager.MOTD_MODULE.getModuleSection().getStringList("message");
+            motd.stream().forEach(message -> MessagesService.sendMessage(player, PlaceHolderUtil.formatMessage(message, new BungeeChatContext(bungeeChatAccount))));
+
+        }
     }
 
 }
