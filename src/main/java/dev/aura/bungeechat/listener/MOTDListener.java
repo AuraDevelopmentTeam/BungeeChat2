02844e25782f00f863e6c8cfb6f0bd6cf917e11e
BrainStone <yannick@tedworld.de>
2017-07-02 00:10:07 +0200

    Using proper join logic

--- src/main/java/dev/aura/bungeechat/listener/MOTDListener.java
+++ src/main/java/dev/aura/bungeechat/listener/MOTDListener.java
@@ -1,6 +1,8 @@
 package dev.aura.bungeechat.listener;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import dev.aura.bungeechat.account.BungeecordAccountManager;
@@ -10,20 +12,35 @@ import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.message.PlaceHolderUtil;
 import dev.aura.bungeechat.module.BungeecordModuleManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
 public class MOTDListener implements Listener {
+    private static final List<UUID> joinedPlayers = new LinkedList<>();
+
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerJoin(PostLoginEvent e) {
+    public void onPlayerServerSwitch(ServerSwitchEvent e) {
         ProxiedPlayer player = e.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        if (joinedPlayers.contains(uuid))
+            return;
+
+        joinedPlayers.add(uuid);
+
         BungeeChatAccount bungeeChatAccount = BungeecordAccountManager.getAccount(player).get();
         List<String> motd = BungeecordModuleManager.MOTD_MODULE.getModuleSection().getStringList("message");
 
         String message = motd.stream().collect(Collectors.joining("\n"));
         MessagesService.sendMessage(player,
                 PlaceHolderUtil.formatMessage(message, new BungeeChatContext(bungeeChatAccount)));
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerDisconnectEvent e) {
+        joinedPlayers.remove(e.getPlayer().getUniqueId());
     }
 }
