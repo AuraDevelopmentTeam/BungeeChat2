1721b368dd75f249a783a30271b668148104bdb3
BrainStone <yannick@tedworld.de>
2017-07-02 00:07:43 +0200

    Improved MOTD

--- src/main/java/dev/aura/bungeechat/listener/MOTDListener.java
+++ src/main/java/dev/aura/bungeechat/listener/MOTDListener.java
@@ -1,5 +1,8 @@
 package dev.aura.bungeechat.listener;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import dev.aura.bungeechat.account.BungeecordAccountManager;
 import dev.aura.bungeechat.api.account.BungeeChatAccount;
 import dev.aura.bungeechat.api.placeholder.BungeeChatContext;
@@ -12,16 +15,15 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
-import java.util.ArrayList;
-
 public class MOTDListener implements Listener {
-
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PostLoginEvent e) {
         ProxiedPlayer player = e.getPlayer();
         BungeeChatAccount bungeeChatAccount = BungeecordAccountManager.getAccount(player).get();
-        ArrayList<String> motd = (ArrayList<String>) BungeecordModuleManager.MOTD_MODULE.getModuleSection().getStringList("message");
-        motd.stream().forEach(message -> MessagesService.sendMessage(player, PlaceHolderUtil.formatMessage(message, new BungeeChatContext(bungeeChatAccount))));
-    }
+        List<String> motd = BungeecordModuleManager.MOTD_MODULE.getModuleSection().getStringList("message");
 
+        String message = motd.stream().collect(Collectors.joining("\n"));
+        MessagesService.sendMessage(player,
+                PlaceHolderUtil.formatMessage(message, new BungeeChatContext(bungeeChatAccount)));
+    }
 }
