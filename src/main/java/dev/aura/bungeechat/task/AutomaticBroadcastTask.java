b11af7e0a98cfba23769e0e3c6921919e8569ad5
BrainStone <yannick@tedworld.de>
2017-07-03 15:43:28 +0200

    Improved auto broadcast

--- src/main/java/dev/aura/bungeechat/task/AutomaticBroadcastTask.java
+++ src/main/java/dev/aura/bungeechat/task/AutomaticBroadcastTask.java
@@ -1,39 +1,44 @@
 package dev.aura.bungeechat.task;
 
-import dev.aura.bungeechat.BungeeChat;
+import java.util.List;
+import java.util.Random;
+
 import dev.aura.bungeechat.api.placeholder.BungeeChatContext;
 import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.message.PlaceHolderUtil;
-import dev.aura.bungeechat.module.BungeecordModuleManager;
-import net.md_5.bungee.api.ProxyServer;
 
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+public class AutomaticBroadcastTask implements Runnable {
+    private final List<String> messages;
+    private final int size;
+    private final boolean random;
+    private int current;
+    private Random rand;
 
-public class AutomaticBroadcastTask implements RepeatingTask {
-
-    private static ArrayList<String> messages;
-    private static int current = 0;
-
-    @Override
-    public void start() {
-        if (BungeecordModuleManager.ALERT_MODULE.getModuleSection().getBoolean("automaticBroadcast.enabled")) {
-            messages = (ArrayList<String>) BungeecordModuleManager.ALERT_MODULE.getModuleSection().getStringList("automaticBroadcast.messages");
-            int interval = BungeecordModuleManager.ALERT_MODULE.getModuleSection().getInt("automaticBroadcast.interval");
-            ProxyServer.getInstance().getScheduler().schedule(BungeeChat.getInstance(),
-                    () -> {
-                        MessagesService.sendToMatchingPlayers(PlaceHolderUtil.formatMessage(messages.get(current), new BungeeChatContext()));
-                        next();
-                    }, interval, interval, TimeUnit.SECONDS);
-        }
+    public AutomaticBroadcastTask(List<String> messages, boolean random) {
+        this.messages = messages;
+        size = messages.size();
+        this.random = random;
+        current = -1;
+        rand = new Random();
     }
 
     @Override
-    public void next() {
-        current++;
-        if ((current + 1) > (messages.size())) {
-            current = 0;
-        }
+    public void run() {
+        MessagesService.sendToMatchingPlayers(PlaceHolderUtil.formatMessage(getMessage(), new BungeeChatContext()),
+                MessagesService.getGlobalPredicate());
     }
 
+    private String getMessage() {
+        if (random) {
+            current = rand.nextInt(size);
+        } else {
+            current++;
+
+            if (current >= size) {
+                current = 0;
+            }
+        }
+
+        return messages.get(current);
+    }
 }
--- src/main/java/dev/aura/bungeechat/task/RepeatingTask.java
+++
@@ -1,8 +0,0 @@
-package dev.aura.bungeechat.task;
-
-public interface RepeatingTask {
-
-    void start();
-    void next();
-
-}
