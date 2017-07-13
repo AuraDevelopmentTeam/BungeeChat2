96fe9e4a48886b27cb9e511d84da083b0e1741c9
BrainStone <yannick@tedworld.de>
2017-07-13 23:49:57 +0200

    Adjusted code to new bStats version

--- src/main/java/dev/aura/bungeechat/hook/metrics/ModuleData.java
+++ src/main/java/dev/aura/bungeechat/hook/metrics/ModuleData.java
@@ -1,20 +1,19 @@
 package dev.aura.bungeechat.hook.metrics;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
-import org.bstats.Metrics.SimpleBarChart;
+import org.bstats.bungeecord.Metrics.SimpleBarChart;
 
 import dev.aura.bungeechat.module.BungeecordModuleManager;
 
 public class ModuleData extends SimpleBarChart {
     public ModuleData() {
-        super("modules");
+        super("modules", ModuleData::getMap);
     }
 
-    @Override
-    public HashMap<String, Integer> getValues(HashMap<String, Integer> premadeMap) {
-        HashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+    public static Map<String, Integer> getMap() {
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
 
         BungeecordModuleManager.getLocalModules().stream()
                 .forEach(module -> sortedMap.put(module.getName(), module.isEnabled() ? 1 : 0));
