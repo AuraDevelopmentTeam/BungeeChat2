c8db1d5b9551e40d0c20fc821bb3918080896338
BrainStone <yannick@tedworld.de>
2017-09-06 11:59:59 +0200

    Added check to see if player is already on
    - Fixes #17

--- src/main/java/dev/aura/bungeechat/event/BungeeChatEventsListener.java
+++ src/main/java/dev/aura/bungeechat/event/BungeeChatEventsListener.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -14,18 +15,31 @@ import net.md_5.bungee.event.EventPriority;
 
 public class BungeeChatEventsListener implements Listener {
     private static final List<UUID> joinedPlayers = new LinkedList<>();
+    private static final List<UUID> duplicatePlayers = new LinkedList<>();
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PostLoginEvent e) {
+        UUID uuid = e.getPlayer().getUniqueId();
+
+        if (!joinedPlayers.contains(uuid))
+            return;
+
+        duplicatePlayers.add(uuid);
+    }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerServerSwitch(ServerSwitchEvent e) {
         ProxiedPlayer player = e.getPlayer();
         UUID uuid = player.getUniqueId();
 
-        if (joinedPlayers.contains(uuid)) {
-            ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatServerSwitchEvent(player));
-        } else {
-            joinedPlayers.add(uuid);
+        if (!duplicatePlayers.contains(uuid)) {
+            if (joinedPlayers.contains(uuid)) {
+                ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatServerSwitchEvent(player));
+            } else {
+                joinedPlayers.add(uuid);
 
-            ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatJoinEvent(player));
+                ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatJoinEvent(player));
+            }
         }
     }
 
@@ -37,8 +51,12 @@ public class BungeeChatEventsListener implements Listener {
         if (!joinedPlayers.contains(uuid))
             return;
 
-        joinedPlayers.remove(uuid);
+        if (duplicatePlayers.contains(uuid)) {
+            duplicatePlayers.remove(uuid);
+        } else {
+            joinedPlayers.remove(uuid);
 
-        ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatLeaveEvent(player));
+            ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatLeaveEvent(player));
+        }
     }
 }
