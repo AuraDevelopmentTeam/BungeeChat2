ab87e2d061aabafe9b3e32c5352dfb656c5496d4
BrainStone <yannick@tedworld.de>
2018-03-14 02:56:41 +0100

    Fixed issue with special characters

--- BungeeChatApi/src/test/java/dev/aura/bungeechat/api/util/RegexUtilTest.java
+++ BungeeChatApi/src/test/java/dev/aura/bungeechat/api/util/RegexUtilTest.java
@@ -45,5 +45,25 @@ public class RegexUtilTest {
     }
   }
 
+  @Test
+  public void specialChars() {
+    final String testString = "dhäöüßÄÖÜàÎ学校hg";
+    Pattern pattern;
+
+    // Iterate over all possible flag combinations
+    for (int i = 0; i < (1 << 4); ++i) {
+      pattern =
+          RegexUtil.parseWildcardToPattern(
+              testString,
+              Pattern.CASE_INSENSITIVE,
+              (i & (1 << 0)) > 0,
+              (i & (1 << 1)) > 0,
+              (i & (1 << 2)) > 0,
+              (i & (1 << 3)) > 0);
+
+      assertTrue("Pattern doesn't match itself!", pattern.matcher(testString).matches());
+    }
+  }
+
   // TODO: More tests!
 }
