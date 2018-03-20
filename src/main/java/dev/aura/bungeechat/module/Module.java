9e654536fb562c039061f163b3c3939810183807
BrainStone <yannick@tedworld.de>
2018-03-20 06:30:00 +0100

    Fixed reloading issues

--- src/main/java/dev/aura/bungeechat/module/Module.java
+++ src/main/java/dev/aura/bungeechat/module/Module.java
@@ -3,21 +3,17 @@ package dev.aura.bungeechat.module;
 import com.typesafe.config.Config;
 import dev.aura.bungeechat.api.module.BungeeChatModule;
 import dev.aura.bungeechat.config.Configuration;
-import lombok.Getter;
 
 public abstract class Module implements BungeeChatModule {
   public static final String MODULE_BASE = "Modules";
   public static final String CONFIG_ENABLED = "enabled";
-
-  @Getter(lazy = true)
-  private final Config moduleSection = generateModuleSection();
 
   @Override
   public boolean isEnabled() {
     return getModuleSection().getBoolean(CONFIG_ENABLED);
   }
 
-  private Config generateModuleSection() {
+  public Config getModuleSection() {
     return Configuration.get().getConfig(MODULE_BASE).getConfig(getName());
   }
 }
