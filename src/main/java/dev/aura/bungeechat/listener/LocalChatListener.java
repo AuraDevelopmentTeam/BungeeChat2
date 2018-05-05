87b65d5b8135827f0fbc684dce1fd6fd48e550b0
Brian Ronald <brian@ppcis.org>
2018-05-06 00:06:28 +0100

    Consult the config in the chat listener, and choose whether to cancel and/or send locally
    I wanted to do this in the Predicate, but I ran into trouble.

--- src/main/java/dev/aura/bungeechat/listener/LocalChatListener.java
+++ src/main/java/dev/aura/bungeechat/listener/LocalChatListener.java
@@ -1,5 +1,6 @@
 package dev.aura.bungeechat.listener;
 
+import com.typesafe.config.Config;
 import dev.aura.bungeechat.account.BungeecordAccountManager;
 import dev.aura.bungeechat.api.account.BungeeChatAccount;
 import dev.aura.bungeechat.api.enums.ChannelType;
@@ -12,9 +13,17 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
+import java.util.List;
+
 public class LocalChatListener implements Listener {
   private final boolean passToClientServer =
       BungeecordModuleManager.LOCAL_CHAT_MODULE.getModuleSection().getBoolean("passToClientServer");
+  private final boolean passTransparently =
+      BungeecordModuleManager.LOCAL_CHAT_MODULE.getModuleSection().getBoolean("passTransparently");
+  private final Config serverListSection =
+          BungeecordModuleManager.LOCAL_CHAT_MODULE.getModuleSection().getConfig("serverList");
+  private final boolean serverListDisabled = !serverListSection.getBoolean("enabled");
+  private final List<String> passthruServers = serverListSection.getStringList("list");
 
   @EventHandler(priority = EventPriority.HIGHEST)
   public void onPlayerChat(ChatEvent e) {
@@ -28,8 +37,10 @@ public class LocalChatListener implements Listener {
     if (ChatUtils.isCommand(message)) return;
 
     if (account.getChannelType() == ChannelType.LOCAL) {
-      e.setCancelled(!passToClientServer);
-      MessagesService.sendLocalMessage(sender, message);
+      // Check we send to this server
+      e.setCancelled(!(passToClientServer && (serverListDisabled || passthruServers.contains(account.getServerName()))));
+      // Was just cancelled, or we want to process all local chat regardless
+      if (e.isCancelled() || !passTransparently) MessagesService.sendLocalMessage(sender, message);
     }
   }
 }
