4da45c32ed56e212fbbe18155a12435df2d5ec90
BrainStone <yannick@tedworld.de>
2020-04-03 23:41:39 +0200

    Removed pointless player check
    - Also using short cicuiting

--- src/main/java/dev/aura/bungeechat/command/ChatLockCommand.java
+++ src/main/java/dev/aura/bungeechat/command/ChatLockCommand.java
@@ -9,7 +9,6 @@ import dev.aura.bungeechat.module.ChatLockModule;
 import dev.aura.bungeechat.permission.Permission;
 import dev.aura.bungeechat.permission.PermissionManager;
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class ChatLockCommand extends BaseCommand {
   private static final String USAGE = "/chatlock <local|global> [clear]";
@@ -20,56 +19,52 @@ public class ChatLockCommand extends BaseCommand {
 
   @Override
   public void execute(CommandSender sender, String[] args) {
-    if (PermissionManager.hasPermission(sender, Permission.COMMAND_CHAT_LOCK)) {
-      if (!(sender instanceof ProxiedPlayer)) {
-        MessagesService.sendMessage(sender, Messages.NOT_A_PLAYER.get());
+    // The permission check sends the no permission message
+    if (!PermissionManager.hasPermission(sender, Permission.COMMAND_CHAT_LOCK)) return;
+
+    BungeeChatAccount player = BungeecordAccountManager.getAccount(sender).get();
+
+    if (args.length < 1) {
+      MessagesService.sendMessage(sender, Messages.INCORRECT_USAGE.get(player, USAGE));
+      return;
+    }
+
+    final ChatLockModule chatLock = BungeecordModuleManager.CHAT_LOCK_MODULE;
+    final boolean clear = (args.length >= 2) && args[1].equalsIgnoreCase("clear");
+    final int emptyLines = clear ? chatLock.getModuleSection().getInt("emptyLinesOnClear") : 0;
+
+    if (args[0].equalsIgnoreCase("global")) {
+      if (chatLock.isGlobalChatLockEnabled()) {
+        chatLock.disableGlobalChatLock();
+        MessagesService.sendMessage(sender, Messages.DISABLE_CHATLOCK.get(player));
       } else {
-        BungeeChatAccount player = BungeecordAccountManager.getAccount(sender).get();
+        chatLock.enableGlobalChatLock();
 
-        if (args.length < 1) {
-          MessagesService.sendMessage(sender, Messages.INCORRECT_USAGE.get(player, USAGE));
-        } else {
-          final ChatLockModule chatLock = BungeecordModuleManager.CHAT_LOCK_MODULE;
-          final boolean clear = (args.length >= 2) && args[1].equalsIgnoreCase("clear");
-          final int emptyLines =
-              clear ? chatLock.getModuleSection().getInt("emptyLinesOnClear") : 0;
-
-          if (args[0].equalsIgnoreCase("global")) {
-            if (chatLock.isGlobalChatLockEnabled()) {
-              chatLock.disableGlobalChatLock();
-              MessagesService.sendMessage(sender, Messages.DISABLE_CHATLOCK.get(player));
-            } else {
-              chatLock.enableGlobalChatLock();
-
-              if (clear) {
-                ClearChatCommand.clearGlobalChat(emptyLines);
-              }
-
-              MessagesService.sendToMatchingPlayers(
-                  Messages.ENABLE_CHATLOCK.get(player), MessagesService.getGlobalPredicate());
-            }
-          } else if (args[0].equalsIgnoreCase("local")) {
-            String serverName = player.getServerName();
-
-            if (chatLock.isLocalChatLockEnabled(serverName)) {
-              chatLock.disableLocalChatLock(serverName);
-              MessagesService.sendMessage(sender, Messages.DISABLE_CHATLOCK.get(player));
-            } else {
-              chatLock.enableLocalChatLock(serverName);
-
-              if (clear) {
-                ClearChatCommand.clearLocalChat(serverName, emptyLines);
-              }
-
-              MessagesService.sendToMatchingPlayers(
-                  Messages.ENABLE_CHATLOCK.get(player),
-                  MessagesService.getLocalPredicate(serverName));
-            }
-          } else {
-            MessagesService.sendMessage(sender, Messages.INCORRECT_USAGE.get(player, USAGE));
-          }
+        if (clear) {
+          ClearChatCommand.clearGlobalChat(emptyLines);
         }
+
+        MessagesService.sendToMatchingPlayers(
+            Messages.ENABLE_CHATLOCK.get(player), MessagesService.getGlobalPredicate());
       }
+    } else if (args[0].equalsIgnoreCase("local")) {
+      String serverName = player.getServerName();
+
+      if (chatLock.isLocalChatLockEnabled(serverName)) {
+        chatLock.disableLocalChatLock(serverName);
+        MessagesService.sendMessage(sender, Messages.DISABLE_CHATLOCK.get(player));
+      } else {
+        chatLock.enableLocalChatLock(serverName);
+
+        if (clear) {
+          ClearChatCommand.clearLocalChat(serverName, emptyLines);
+        }
+
+        MessagesService.sendToMatchingPlayers(
+            Messages.ENABLE_CHATLOCK.get(player), MessagesService.getLocalPredicate(serverName));
+      }
+    } else {
+      MessagesService.sendMessage(sender, Messages.INCORRECT_USAGE.get(player, USAGE));
     }
   }
 }
