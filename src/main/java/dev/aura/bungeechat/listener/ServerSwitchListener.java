a0048feb6a1851c43ff22de302b96af55b801cd9
BrainStone <yannick@tedworld.de>
2020-04-24 21:04:20 +0200

    Prevent isues with older BungeeCord versions

--- src/main/java/dev/aura/bungeechat/listener/ServerSwitchListener.java
+++ src/main/java/dev/aura/bungeechat/listener/ServerSwitchListener.java
@@ -4,17 +4,26 @@ import dev.aura.bungeechat.event.BungeeChatServerSwitchEvent;
 import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.permission.Permission;
 import dev.aura.bungeechat.permission.PermissionManager;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class ServerSwitchListener implements Listener {
   @EventHandler
-  public void onPlayerServerSwitch(BungeeChatServerSwitchEvent e) {
-    ProxiedPlayer player = e.getPlayer();
+  public void onPlayerServerSwitch(BungeeChatServerSwitchEvent event) {
+    ProxiedPlayer player = event.getPlayer();
 
     if (PermissionManager.hasPermission(player, Permission.MESSAGE_SWITCH)) {
-      MessagesService.sendSwitchMessage(player, e.getFrom());
+      MessagesService.sendSwitchMessage(player, getServerInfo(event));
+    }
+  }
+
+  private static ServerInfo getServerInfo(BungeeChatServerSwitchEvent event) {
+    try {
+      return event.getFrom();
+    } catch (NoSuchMethodError e) {
+      return null;
     }
   }
 }
