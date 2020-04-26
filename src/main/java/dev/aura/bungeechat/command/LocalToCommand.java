f605f89155cbdd027f8b7733c45ed347030b0a6e
gb2233 <gb2233@users.noreply.github.com>
2020-04-26 02:27:27 +0200

    Fixed merge errors Fixed Config format

--- src/main/java/dev/aura/bungeechat/command/LocalToCommand.java
+++ src/main/java/dev/aura/bungeechat/command/LocalToCommand.java
@@ -1,60 +1,50 @@
 package dev.aura.bungeechat.command;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-import dev.aura.bungeechat.account.BungeecordAccountManager;
-import dev.aura.bungeechat.api.account.BungeeChatAccount;
-import dev.aura.bungeechat.command.BaseCommand;
 import dev.aura.bungeechat.message.Context;
-import dev.aura.bungeechat.message.Message;
+import dev.aura.bungeechat.message.Messages;
 import dev.aura.bungeechat.message.MessagesService;
+import dev.aura.bungeechat.module.LocalToModule;
 import dev.aura.bungeechat.permission.Permission;
 import dev.aura.bungeechat.permission.PermissionManager;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-import dev.aura.bungeechat.module.LocalToModule;
 
 public class LocalToCommand extends BaseCommand {
 
-	public LocalToCommand(LocalToModule localToModule) {
-		super("localto", localToModule.getModuleSection().getStringList("aliases"));
-	}
+  public LocalToCommand(LocalToModule localToModule) {
+    super("localto", localToModule.getModuleSection().getStringList("aliases"));
+  }
 
-	@Override
-	public void execute(CommandSender sender, String[] args) {
-		if (PermissionManager.hasPermission(sender, Permission.COMMAND_LOCALTO)) {
-			if (!(sender instanceof ProxiedPlayer)) {
-                MessagesService.sendMessage(sender, Message.NOT_A_PLAYER.get());
-                return;
-            }
-			
-			if (args.length < 2) {
-				MessagesService.sendMessage(sender, Message.INCORRECT_USAGE.get(sender, "/localto <server> <message>"));
-				return;
-			}
+  @Override
+  public void execute(CommandSender sender, String[] args) {
+    if (PermissionManager.hasPermission(sender, Permission.COMMAND_LOCALTO)) {
 
-			String server = args[0];
-			boolean serverExists = false;
-			for (ServerInfo serverInfo : ProxyServer.getInstance().getServers().values()) {
-				if (serverInfo.getName().equalsIgnoreCase(server)) {
-					serverExists = true;
-					server = serverInfo.getName();
-					break;
-				}
-			}
-			
-			if (!serverExists) {
-				MessagesService.sendMessage(sender, Message.UNKNOWN_SERVER.get(sender, server));
-				return;
-			}
-			
-			String finalMessage = Arrays.stream(args).skip(1).collect(Collectors.joining(" "));
-			MessagesService.sendLocalMessage(new Context(sender, finalMessage, server));
-		}
+      if (args.length < 2) {
+        MessagesService.sendMessage(
+            sender, Messages.INCORRECT_USAGE.get(sender, "/localto <server> <message>"));
+        return;
+      }
 
-	}
+      String server = args[0];
+      boolean serverExists = false;
+      for (ServerInfo serverInfo : ProxyServer.getInstance().getServers().values()) {
+        if (serverInfo.getName().equalsIgnoreCase(server)) {
+          serverExists = true;
+          server = serverInfo.getName();
+          break;
+        }
+      }
 
+      if (!serverExists) {
+        MessagesService.sendMessage(sender, Messages.UNKNOWN_SERVER.get(sender, server));
+        return;
+      }
+
+      String finalMessage = Arrays.stream(args).skip(1).collect(Collectors.joining(" "));
+      MessagesService.sendLocalMessage(new Context(sender, finalMessage, server));
+    }
+  }
 }
--- src/main/java/dev/aura/bungeechat/module/LocalToModule.java
+++ src/main/java/dev/aura/bungeechat/module/LocalToModule.java
@@ -1,30 +1,29 @@
 package dev.aura.bungeechat.module;
 
 import dev.aura.bungeechat.BungeeChat;
-import dev.aura.bungeechat.module.Module;
-import net.md_5.bungee.api.ProxyServer;
 import dev.aura.bungeechat.command.LocalToCommand;
+import net.md_5.bungee.api.ProxyServer;
 
 public class LocalToModule extends Module {
 
-    private LocalToCommand localToCommand;
-    
-    @Override
-    public String getName() {
-        return "LocalTo";
-    }
-    
-    @Override
-    public void onEnable() {
-        ProxyServer.getInstance().getLogger().info("Enable localtocommand");
-        localToCommand = new LocalToCommand(this);
-        
-        ProxyServer.getInstance().getPluginManager().registerCommand(BungeeChat.getInstance(), localToCommand);
-    }
-    
-    @Override
-    public void onDisable() {
-        ProxyServer.getInstance().getPluginManager().unregisterCommand(localToCommand);
-    }
-    
+  private LocalToCommand localToCommand;
+
+  @Override
+  public String getName() {
+    return "LocalTo";
+  }
+
+  @Override
+  public void onEnable() {
+    localToCommand = new LocalToCommand(this);
+
+    ProxyServer.getInstance()
+        .getPluginManager()
+        .registerCommand(BungeeChat.getInstance(), localToCommand);
+  }
+
+  @Override
+  public void onDisable() {
+    ProxyServer.getInstance().getPluginManager().unregisterCommand(localToCommand);
+  }
 }
