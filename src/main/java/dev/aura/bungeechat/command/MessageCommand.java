ab400dfc302b44131e675f74e69c3fd8e6b10162
BrainStone <yannick@tedworld.de>
2020-04-30 22:40:13 +0200

    Added tabComplete to /msg and /mute
    - More early exit code

--- src/main/java/dev/aura/bungeechat/command/MessageCommand.java
+++ src/main/java/dev/aura/bungeechat/command/MessageCommand.java
@@ -9,6 +9,7 @@ import dev.aura.bungeechat.module.MessengerModule;
 import dev.aura.bungeechat.permission.Permission;
 import dev.aura.bungeechat.permission.PermissionManager;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import net.md_5.bungee.api.CommandSender;
@@ -20,37 +21,47 @@ public class MessageCommand extends BaseCommand {
 
   @Override
   public void execute(CommandSender sender, String[] args) {
-    if (PermissionManager.hasPermission(sender, Permission.COMMAND_MESSAGE)) {
-      if (args.length < 2) {
-        MessagesService.sendMessage(
-            sender, Messages.INCORRECT_USAGE.get(sender, "/msg <player> <message>"));
-      } else {
-        Optional<BungeeChatAccount> targetAccount = AccountManager.getAccount(args[0]);
+    if (!PermissionManager.hasPermission(sender, Permission.COMMAND_MESSAGE)) return;
+    if (args.length < 2) {
+      MessagesService.sendMessage(
+          sender, Messages.INCORRECT_USAGE.get(sender, "/msg <player> <message>"));
+    } else {
+      Optional<BungeeChatAccount> targetAccount = AccountManager.getAccount(args[0]);
 
-        if (!targetAccount.isPresent()
-            || (targetAccount.get().isVanished()
-                && !PermissionManager.hasPermission(sender, Permission.COMMAND_VANISH_VIEW))) {
-          MessagesService.sendMessage(sender, Messages.PLAYER_NOT_FOUND.get());
-          return;
-        }
-
-        CommandSender target = BungeecordAccountManager.getCommandSender(targetAccount.get()).get();
-
-        if (target == sender) {
-          MessagesService.sendMessage(sender, Messages.MESSAGE_YOURSELF.get());
-          return;
-        }
-        if (!targetAccount.get().hasMessangerEnabled()
-            && !PermissionManager.hasPermission(sender, Permission.BYPASS_TOGGLE_MESSAGE)) {
-          MessagesService.sendMessage(sender, Messages.HAS_MESSAGER_DISABLED.get(target));
-          return;
-        }
-
-        String finalMessage = Arrays.stream(args, 1, args.length).collect(Collectors.joining(" "));
-
-        MessagesService.sendPrivateMessage(sender, target, finalMessage);
-        ReplyCommand.setReply(sender, target);
+      if (!targetAccount.isPresent()
+          || (targetAccount.get().isVanished()
+              && !PermissionManager.hasPermission(sender, Permission.COMMAND_VANISH_VIEW))) {
+        MessagesService.sendMessage(sender, Messages.PLAYER_NOT_FOUND.get());
+        return;
       }
+
+      CommandSender target = BungeecordAccountManager.getCommandSender(targetAccount.get()).get();
+
+      if (target == sender) {
+        MessagesService.sendMessage(sender, Messages.MESSAGE_YOURSELF.get());
+        return;
+      }
+      if (!targetAccount.get().hasMessangerEnabled()
+          && !PermissionManager.hasPermission(sender, Permission.BYPASS_TOGGLE_MESSAGE)) {
+        MessagesService.sendMessage(sender, Messages.HAS_MESSAGER_DISABLED.get(target));
+        return;
+      }
+
+      String finalMessage = Arrays.stream(args, 1, args.length).collect(Collectors.joining(" "));
+
+      MessagesService.sendPrivateMessage(sender, target, finalMessage);
+      ReplyCommand.setReply(sender, target);
     }
+  }
+
+  @Override
+  public Collection<String> tabComplete(CommandSender sender, String[] args) {
+    if (args.length == 1) {
+      return BungeecordAccountManager.getAccountsForPartialName(args[0]).stream()
+          .map(BungeeChatAccount::getName)
+          .collect(Collectors.toList());
+    }
+
+    return super.tabComplete(sender, args);
   }
 }
--- src/main/java/dev/aura/bungeechat/command/MuteCommand.java
+++ src/main/java/dev/aura/bungeechat/command/MuteCommand.java
@@ -8,7 +8,9 @@ import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.module.MutingModule;
 import dev.aura.bungeechat.permission.Permission;
 import dev.aura.bungeechat.permission.PermissionManager;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import net.md_5.bungee.api.CommandSender;
 
 public class MuteCommand extends BaseCommand {
@@ -18,27 +20,38 @@ public class MuteCommand extends BaseCommand {
 
   @Override
   public void execute(CommandSender sender, String[] args) {
-    if (PermissionManager.hasPermission(sender, Permission.COMMAND_MUTE)) {
-      if (args.length < 1) {
-        MessagesService.sendMessage(sender, Messages.INCORRECT_USAGE.get(sender, "/mute <player>"));
-      } else {
-        Optional<BungeeChatAccount> targetAccount = AccountManager.getAccount(args[0]);
+    if (!PermissionManager.hasPermission(sender, Permission.COMMAND_MUTE)) return;
 
-        if (!targetAccount.isPresent()) {
-          MessagesService.sendMessage(sender, Messages.PLAYER_NOT_FOUND.get());
-          return;
-        }
+    if (args.length < 1) {
+      MessagesService.sendMessage(sender, Messages.INCORRECT_USAGE.get(sender, "/mute <player>"));
+    } else {
+      Optional<BungeeChatAccount> targetAccount = AccountManager.getAccount(args[0]);
 
-        CommandSender target = BungeecordAccountManager.getCommandSender(targetAccount.get()).get();
-
-        if (targetAccount.get().isMuted()) {
-          MessagesService.sendMessage(sender, Messages.MUTE_IS_MUTED.get());
-          return;
-        }
-
-        targetAccount.get().mutePermanetly();
-        MessagesService.sendMessage(sender, Messages.MUTE.get(target));
+      if (!targetAccount.isPresent()) {
+        MessagesService.sendMessage(sender, Messages.PLAYER_NOT_FOUND.get());
+        return;
       }
+
+      CommandSender target = BungeecordAccountManager.getCommandSender(targetAccount.get()).get();
+
+      if (targetAccount.get().isMuted()) {
+        MessagesService.sendMessage(sender, Messages.MUTE_IS_MUTED.get());
+        return;
+      }
+
+      targetAccount.get().mutePermanetly();
+      MessagesService.sendMessage(sender, Messages.MUTE.get(target));
     }
+  }
+
+  @Override
+  public Collection<String> tabComplete(CommandSender sender, String[] args) {
+    if (args.length == 1) {
+      return BungeecordAccountManager.getAccountsForPartialName(args[0]).stream()
+          .map(BungeeChatAccount::getName)
+          .collect(Collectors.toList());
+    }
+
+    return super.tabComplete(sender, args);
   }
 }
