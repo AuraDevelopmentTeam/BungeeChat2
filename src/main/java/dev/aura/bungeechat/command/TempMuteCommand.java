b59adf4357c65d8585514ce3506c6c0b73510040
BrainStone <yannick@tedworld.de>
2020-05-02 15:53:24 +0200

    Added TabComplete for TempMute

--- src/main/java/dev/aura/bungeechat/command/TempMuteCommand.java
+++ src/main/java/dev/aura/bungeechat/command/TempMuteCommand.java
@@ -9,10 +9,20 @@ import dev.aura.bungeechat.message.MessagesService;
 import dev.aura.bungeechat.module.MutingModule;
 import dev.aura.bungeechat.permission.Permission;
 import dev.aura.bungeechat.permission.PermissionManager;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import net.md_5.bungee.api.CommandSender;
 
 public class TempMuteCommand extends BaseCommand {
+  private static final List<String> timeUnits = Arrays.asList("s", "m", "h", "d", "w", "mo", "y");
+  private static final Pattern digitsAndUnit =
+      Pattern.compile("(\\d+(?:\\.\\d*)?)[a-z]*", Pattern.CASE_INSENSITIVE);
+
   public TempMuteCommand(MutingModule mutingModule) {
     super(
         "tempmute",
@@ -44,10 +54,41 @@ public class TempMuteCommand extends BaseCommand {
       return;
     }
 
-    double timeAmount = TimeUtil.convertStringTimeToDouble(args[1]),
-        currentTime = System.currentTimeMillis();
-    java.sql.Timestamp timeStamp = new java.sql.Timestamp((long) (currentTime + timeAmount));
+    final double timeAmount = TimeUtil.convertStringTimeToDouble(args[1]);
+    final double currentTime = System.currentTimeMillis();
+    final java.sql.Timestamp timeStamp = new java.sql.Timestamp((long) (currentTime + timeAmount));
     targetAccount.get().setMutedUntil(timeStamp);
     MessagesService.sendMessage(sender, Messages.TEMPMUTE.get(target));
+  }
+
+  @Override
+  public Collection<String> tabComplete(CommandSender sender, String[] args) {
+    if (args.length == 1) {
+      return BungeecordAccountManager.getAccountsForPartialName(args[0]).stream()
+          .map(BungeeChatAccount::getName)
+          .collect(Collectors.toList());
+    } else if (args.length == 2) {
+      final String time = args[1];
+      String digits = null;
+
+      Matcher match = digitsAndUnit.matcher(time);
+
+      if (time.isEmpty()) {
+        digits = "<duration>";
+      } else if (match.matches()) {
+        digits = match.group(1);
+      }
+
+      if (digits != null) {
+        final String finalDigits = digits;
+
+        return timeUnits.stream()
+            .map(unit -> finalDigits + unit)
+            .filter(timeStr -> timeStr.startsWith(time))
+            .collect(Collectors.toList());
+      }
+    }
+
+    return super.tabComplete(sender, args);
   }
 }
