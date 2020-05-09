3d17785be68bdd5fd81b96bdb2b803b3436ad16b
Raymond Xu <raymond.j.xu@gmail.com>
2020-05-09 09:36:27 -0400

    Continue SymbolSubstitution
    This is a draft.

--- src/main/java/dev/aura/bungeechat/filter/SymbolSubstitutionFilter.java
+++ src/main/java/dev/aura/bungeechat/filter/SymbolSubstitutionFilter.java
@@ -4,11 +4,23 @@ import dev.aura.bungeechat.api.account.BungeeChatAccount;
 import dev.aura.bungeechat.api.filter.BlockMessageException;
 import dev.aura.bungeechat.api.filter.BungeeChatFilter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SymbolSubstitutionFilter implements BungeeChatFilter {
+
+    private static Map<String, String> replacementMapping = new HashMap<>();
+    public  SymbolSubstitutionFilter(Map<String, String> replacements) {
+        replacementMapping = replacements;
+    }
+
     @Override
     public String applyFilter(BungeeChatAccount sender, String message) throws BlockMessageException {
-        String finalMessage = message.replaceAll("-->","→");
-        return finalMessage;
+        String finalmessage = message;
+        for (Map.Entry<String, String> entry : replacementMapping.entrySet()) {
+            finalmessage = message.replace(entry.getKey(), entry.getValue());
+        }
+        return finalmessage;
     }
 
     @Override
--- src/main/java/dev/aura/bungeechat/module/SymbolSubstitutionModule.java
+++ src/main/java/dev/aura/bungeechat/module/SymbolSubstitutionModule.java
@@ -1,11 +1,19 @@
 package dev.aura.bungeechat.module;
 
 
+import com.typesafe.config.Config;
 import dev.aura.bungeechat.api.filter.FilterManager;
 import dev.aura.bungeechat.module.Module;
 import dev.aura.bungeechat.filter.SymbolSubstitutionFilter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class SymbolSubstitutionModule extends Module {
+
+    private static Map<String, String> replacementMapping = new HashMap<>();
+
     @Override
     public String getName() {
         return "SymbolSubstitution";
@@ -13,13 +21,15 @@ public class SymbolSubstitutionModule extends Module {
 
     @Override
     public void onEnable() {
-        System.out.println("SymbolSubstitution is enabled!");
-        SymbolSubstitutionFilter symbolSubstitutionFilter = new SymbolSubstitutionFilter();
+        Config section = getModuleSection().getConfig("replacements");
+        replacementMapping =
+                section.root().keySet().stream().collect(Collectors.toMap(key -> key, section::getString));
+        SymbolSubstitutionFilter symbolSubstitutionFilter = new SymbolSubstitutionFilter(replacementMapping);
         FilterManager.addFilter(getName(), symbolSubstitutionFilter);
     }
 
     @Override
     public void onDisable() {
-
+        FilterManager.removeFilter(getName());
     }
 }
