658d3d6ce022327fcc1889fe5b4c051f46269573
BrainStone <yannick@tedworld.de>
2020-05-09 17:02:53 +0200

    Fixed config issue
    - Plus a few other minor things

--- src/main/java/dev/aura/bungeechat/filter/SymbolSubstitutionFilter.java
+++ src/main/java/dev/aura/bungeechat/filter/SymbolSubstitutionFilter.java
@@ -3,12 +3,10 @@ package dev.aura.bungeechat.filter;
 import dev.aura.bungeechat.api.account.BungeeChatAccount;
 import dev.aura.bungeechat.api.filter.BlockMessageException;
 import dev.aura.bungeechat.api.filter.BungeeChatFilter;
-import java.util.HashMap;
 import java.util.Map;
 
 public class SymbolSubstitutionFilter implements BungeeChatFilter {
-
-  private static Map<String, String> replacementMapping = new HashMap<>();
+  private final Map<String, String> replacementMapping;
 
   public SymbolSubstitutionFilter(Map<String, String> replacements) {
     replacementMapping = replacements;
@@ -17,9 +15,11 @@ public class SymbolSubstitutionFilter implements BungeeChatFilter {
   @Override
   public String applyFilter(BungeeChatAccount sender, String message) throws BlockMessageException {
     String finalMessage = message;
+
     for (Map.Entry<String, String> entry : replacementMapping.entrySet()) {
       finalMessage = finalMessage.replace(entry.getKey(), entry.getValue());
     }
+
     return finalMessage;
   }
 
--- src/main/java/dev/aura/bungeechat/module/SymbolSubstitutionModule.java
+++ src/main/java/dev/aura/bungeechat/module/SymbolSubstitutionModule.java
@@ -3,14 +3,10 @@ package dev.aura.bungeechat.module;
 import com.typesafe.config.Config;
 import dev.aura.bungeechat.api.filter.FilterManager;
 import dev.aura.bungeechat.filter.SymbolSubstitutionFilter;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SymbolSubstitutionModule extends Module {
-
-  private static Map<String, String> replacementMapping = new HashMap<>();
-
   @Override
   public String getName() {
     return "SymbolSubstitution";
@@ -18,14 +14,14 @@ public class SymbolSubstitutionModule extends Module {
 
   @Override
   public void onEnable() {
-    Config section = getModuleSection();
-    replacementMapping =
-        section.getObject("replacements").keySet().stream()
-            .collect(Collectors.toMap(key -> key, section::getString));
-    System.out.println(section.getObject("replacements").keySet());
-    SymbolSubstitutionFilter symbolSubstitutionFilter =
-        new SymbolSubstitutionFilter(replacementMapping);
-    FilterManager.addFilter(getName(), symbolSubstitutionFilter);
+    Config replacements = getModuleSection().getConfig("replacements");
+    Map<String, String> replacementMapping =
+        replacements.root().keySet().stream()
+            .collect(Collectors.toMap(key -> key, replacements::getString));
+
+    System.out.println(replacements.root().keySet());
+
+    FilterManager.addFilter(getName(), new SymbolSubstitutionFilter(replacementMapping));
   }
 
   @Override
