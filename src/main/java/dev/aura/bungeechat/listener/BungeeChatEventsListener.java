d923abf9e7556eab343a1c8a17e291cfaa1ecc7e
BrainStone <yannick@tedworld.de>
2020-12-20 21:45:40 +0100

    Use a normal `WeakHashMap`
    `new MapMaker().weakValues().makeMap()` returns a concurrent weak map here. Which is abolutely not desired as the synchronization methods may interfere. That's why I chose to use a normal `WeakHashMap` instead.
    Also did some formatting and clean up. For example you checked 3 times if the `connection` is (not) `null`. Checking once and storing in a variable is much better.
    And the formatting was mostly about leaving space, and using consistent formatting of the conditions.
    Lastly some finals to indicate that we will not change the variables.

--- src/main/java/dev/aura/bungeechat/listener/BungeeChatEventsListener.java
+++ src/main/java/dev/aura/bungeechat/listener/BungeeChatEventsListener.java
@@ -1,11 +1,11 @@
 package dev.aura.bungeechat.listener;
 
-import com.google.common.collect.MapMaker;
 import dev.aura.bungeechat.event.BungeeChatJoinEvent;
 import dev.aura.bungeechat.event.BungeeChatLeaveEvent;
 import dev.aura.bungeechat.event.BungeeChatServerSwitchEvent;
 import java.util.Map;
 import java.util.UUID;
+import java.util.WeakHashMap;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
@@ -15,45 +15,45 @@ import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
 public class BungeeChatEventsListener implements Listener {
-  private static final Map<UUID, ProxiedPlayer> primaryConnections =
-      new MapMaker().weakValues().makeMap();
+  private static final Map<UUID, ProxiedPlayer> primaryConnections = new WeakHashMap<>();
 
   @EventHandler(priority = EventPriority.HIGHEST)
   public void onPlayerServerSwitch(ServerSwitchEvent e) {
-    ProxiedPlayer player = e.getPlayer();
-    UUID uuid = player.getUniqueId();
+    final boolean isNewConnection;
+    final ProxiedPlayer player = e.getPlayer();
+    final UUID uuid = player.getUniqueId();
 
-    boolean isServerSwitch;
     synchronized (primaryConnections) {
-      ProxiedPlayer connection = primaryConnections.getOrDefault(uuid, null);
-      if (connection != null && connection != player) {
-        // player is already connected through another connection and join event was called
-        return;
-      }
-      isServerSwitch = connection != null;
-      if (connection == null)
-        primaryConnections.put(uuid, player); // set current connection as primary for this uuid
+      final ProxiedPlayer connection = primaryConnections.get(uuid);
+      isNewConnection = connection == null;
+
+      // player is already connected through another connection and join event was called
+      if (!isNewConnection && connection != player) return;
+
+      // set current connection as primary for this uuid
+      if (isNewConnection) primaryConnections.put(uuid, player);
     }
 
-    if (isServerSwitch) {
+    if (isNewConnection) {
+      ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatJoinEvent(player));
+    } else {
       ProxyServer.getInstance()
           .getPluginManager()
           .callEvent(new BungeeChatServerSwitchEvent(player, e));
-    } else {
-      ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatJoinEvent(player));
     }
   }
 
   @EventHandler(priority = EventPriority.HIGH)
   public void onPlayerLeave(PlayerDisconnectEvent e) {
-    ProxiedPlayer player = e.getPlayer();
-    UUID uuid = player.getUniqueId();
+    final ProxiedPlayer player = e.getPlayer();
+    final UUID uuid = player.getUniqueId();
 
     synchronized (primaryConnections) {
-      ProxiedPlayer connection = primaryConnections.getOrDefault(uuid, null);
-      if (connection
-          != player) // call BungeeChatLeaveEvent only when primary connection is disconnected
-      return;
+      final ProxiedPlayer connection = primaryConnections.get(uuid);
+
+      // call BungeeChatLeaveEvent only when primary connection is disconnected
+      if (connection != player) return;
+
       primaryConnections.remove(uuid);
     }
 
