90e8dcfea37fe34360cf2c5668bb9b6b5dc6c494
BrainStone <yannick@tedworld.de>
2020-12-21 03:10:06 +0100

    Use `WeakReference`s and a normal `HashMap`
    A WeakHashMap has weak keys, not weak values. So we use a regular HashMap with WeakReferences instead. Works just as well but we need to clean after ourselves.

--- src/main/java/dev/aura/bungeechat/listener/BungeeChatEventsListener.java
+++ src/main/java/dev/aura/bungeechat/listener/BungeeChatEventsListener.java
@@ -3,9 +3,10 @@ package dev.aura.bungeechat.listener;
 import dev.aura.bungeechat.event.BungeeChatJoinEvent;
 import dev.aura.bungeechat.event.BungeeChatLeaveEvent;
 import dev.aura.bungeechat.event.BungeeChatServerSwitchEvent;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.WeakHashMap;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
@@ -15,7 +16,7 @@ import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
 public class BungeeChatEventsListener implements Listener {
-  private static final Map<UUID, ProxiedPlayer> primaryConnections = new WeakHashMap<>();
+  private static final Map<UUID, WeakReference<ProxiedPlayer>> primaryConnections = new HashMap<>();
 
   @EventHandler(priority = EventPriority.HIGHEST)
   public void onPlayerServerSwitch(ServerSwitchEvent e) {
@@ -24,14 +25,15 @@ public class BungeeChatEventsListener implements Listener {
     final UUID uuid = player.getUniqueId();
 
     synchronized (primaryConnections) {
-      final ProxiedPlayer connection = primaryConnections.get(uuid);
+      final ProxiedPlayer connection =
+          primaryConnections.getOrDefault(uuid, new WeakReference<>(null)).get();
       isNewConnection = connection == null;
 
       // player is already connected through another connection and join event was called
-      if (!isNewConnection && connection != player) return;
+      if (!isNewConnection && (connection != player)) return;
 
       // set current connection as primary for this uuid
-      if (isNewConnection) primaryConnections.put(uuid, player);
+      if (isNewConnection) primaryConnections.put(uuid, new WeakReference<>(player));
     }
 
     if (isNewConnection) {
@@ -49,12 +51,15 @@ public class BungeeChatEventsListener implements Listener {
     final UUID uuid = player.getUniqueId();
 
     synchronized (primaryConnections) {
-      final ProxiedPlayer connection = primaryConnections.get(uuid);
+      final ProxiedPlayer connection =
+          primaryConnections.getOrDefault(uuid, new WeakReference<>(null)).get();
 
       // call BungeeChatLeaveEvent only when primary connection is disconnected
       if (connection != player) return;
 
       primaryConnections.remove(uuid);
+      // Clean any orphaned entries
+      primaryConnections.values().removeIf(ref -> ref.get() == null);
     }
 
     ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatLeaveEvent(player));
