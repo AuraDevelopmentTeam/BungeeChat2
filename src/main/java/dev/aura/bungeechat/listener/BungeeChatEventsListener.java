25203cc61c00b54037a34141af37ff7b82de0f72
BrainStone <yannick@tedworld.de>
2020-12-22 01:13:12 +0100

    Back to the MapMaker variant as that is cleaner to read
    There may be issues here as `ConcurrentMap`s and `synchronnized` supposedly don't mix well. However I believe this to be fine here. We don't use the concurrency of the map ever so it shouldn't matter.
    Another concern is that the map may be performance heavy.
    
    If any of these concerns turn out to be substancial, we can always just revert this commit.

--- src/main/java/dev/aura/bungeechat/listener/BungeeChatEventsListener.java
+++ src/main/java/dev/aura/bungeechat/listener/BungeeChatEventsListener.java
@@ -1,10 +1,10 @@
 package dev.aura.bungeechat.listener;
 
+import com.google.common.collect.MapMaker;
 import dev.aura.bungeechat.event.BungeeChatJoinEvent;
 import dev.aura.bungeechat.event.BungeeChatLeaveEvent;
 import dev.aura.bungeechat.event.BungeeChatServerSwitchEvent;
-import java.lang.ref.WeakReference;
-import java.util.HashMap;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Map;
 import java.util.UUID;
 import net.md_5.bungee.api.ProxyServer;
@@ -15,8 +15,15 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
+@SuppressFBWarnings(
+    value = "JLM_JSR166_UTILCONCURRENT_MONITORENTER",
+    justification =
+        "Synchronization is correct as is. The map produced by MapMaker is a ConcurrentMap, even"
+            + "thought that concurrency is not being used. There is no way to get a non concurrent"
+            + "map from MapMaker")
 public class BungeeChatEventsListener implements Listener {
-  private static final Map<UUID, WeakReference<ProxiedPlayer>> primaryConnections = new HashMap<>();
+  private static final Map<UUID, ProxiedPlayer> primaryConnections =
+      new MapMaker().weakValues().concurrencyLevel(1).makeMap();
 
   @EventHandler(priority = EventPriority.HIGHEST)
   public void onPlayerServerSwitch(ServerSwitchEvent e) {
@@ -25,15 +32,14 @@ public class BungeeChatEventsListener implements Listener {
     final UUID uuid = player.getUniqueId();
 
     synchronized (primaryConnections) {
-      final ProxiedPlayer connection =
-          primaryConnections.getOrDefault(uuid, new WeakReference<>(null)).get();
+      final ProxiedPlayer connection = primaryConnections.get(uuid);
       isNewConnection = connection == null;
 
       // player is already connected through another connection and join event was called
       if (!isNewConnection && (connection != player)) return;
 
       // set current connection as primary for this uuid
-      if (isNewConnection) primaryConnections.put(uuid, new WeakReference<>(player));
+      if (isNewConnection) primaryConnections.put(uuid, player);
     }
 
     if (isNewConnection) {
@@ -51,15 +57,12 @@ public class BungeeChatEventsListener implements Listener {
     final UUID uuid = player.getUniqueId();
 
     synchronized (primaryConnections) {
-      final ProxiedPlayer connection =
-          primaryConnections.getOrDefault(uuid, new WeakReference<>(null)).get();
+      final ProxiedPlayer connection = primaryConnections.get(uuid);
 
       // call BungeeChatLeaveEvent only when primary connection is disconnected
       if (connection != player) return;
 
       primaryConnections.remove(uuid);
-      // Clean any orphaned entries
-      primaryConnections.values().removeIf(ref -> ref.get() == null);
     }
 
     ProxyServer.getInstance().getPluginManager().callEvent(new BungeeChatLeaveEvent(player));
