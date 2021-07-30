cf385e9a534f35e6c773f7e1b4f8d72d15249149
BrainStone <yannick@tedworld.de>
2021-07-30 05:57:50 +0200

    Add support for CloudNet 3.4

--- src/main/java/dev/aura/bungeechat/hook/CloudNetPerms3Hook.java
+++ src/main/java/dev/aura/bungeechat/hook/CloudNetPerms3Hook.java
@@ -1,7 +1,8 @@
 package dev.aura.bungeechat.hook;
 
+import de.dytanic.cloudnet.driver.CloudNetDriver;
 import de.dytanic.cloudnet.driver.permission.IPermissionGroup;
-import de.dytanic.cloudnet.driver.permission.IPermissionUser;
+import de.dytanic.cloudnet.driver.permission.IPermissionManagement;
 import de.dytanic.cloudnet.ext.cloudperms.CloudPermissionsManagement;
 import dev.aura.bungeechat.api.account.BungeeChatAccount;
 import dev.aura.bungeechat.api.hook.BungeeChatHook;
@@ -10,10 +11,39 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class CloudNetPerms3Hook implements BungeeChatHook {
-  private final CloudPermissionsManagement api;
+  private final IPermissionManagement api;
 
+  @SuppressWarnings("deprecation")
   public CloudNetPerms3Hook() {
-    api = CloudPermissionsManagement.getInstance();
+    IPermissionManagement api = null;
+    Throwable lastException = null;
+
+    try {
+      api = CloudNetDriver.getInstance().getPermissionManagement();
+    } catch (LinkageError | NullPointerException e) {
+      // Fallback for CloudNet 3.2 or lower
+      // api is null. We'll hit the next condition
+      lastException = e;
+    }
+
+    if (api == null) {
+      try {
+        // This is just a fallback
+        //noinspection UnstableApiUsage
+        api = CloudPermissionsManagement.getInstance();
+      } catch (LinkageError | NullPointerException e) {
+        // Still nothing. At least keep the exception
+        lastException = e;
+      }
+
+      if (api == null) {
+        throw new IllegalStateException(
+            "Could not initialize CloudNet 3 due to neither the new, nor the old method working",
+            lastException);
+      }
+    }
+
+    this.api = api;
   }
 
   @Override
@@ -27,10 +57,9 @@ public class CloudNetPerms3Hook implements BungeeChatHook {
   }
 
   private Optional<IPermissionGroup> getPermissionGroup(BungeeChatAccount account) {
-    IPermissionUser player = api.getUser(account.getUniqueId());
-    IPermissionGroup permissionGroup = api.getHighestPermissionGroup(player);
-
-    return Optional.ofNullable(permissionGroup);
+    return Optional.ofNullable(api.getUser(account.getUniqueId()))
+        .map(api::getHighestPermissionGroup)
+        .filter(Objects::nonNull);
   }
 
   @Override
