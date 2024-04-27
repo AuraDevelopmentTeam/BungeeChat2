6bc8e5672f887bb191e15ff16c770e2ca506389c
BrainStone <yannick@tedworld.de>
2024-04-27 02:40:00 +0200

    Allowing multiple versions of the same translation

--- src/main/java/dev/aura/bungeechat/hook/metrics/LanguageData.java
+++ src/main/java/dev/aura/bungeechat/hook/metrics/LanguageData.java
@@ -4,6 +4,7 @@ import com.google.common.annotations.VisibleForTesting;
 import dev.aura.bungeechat.config.Configuration;
 import java.util.Locale;
 import java.util.MissingResourceException;
+import org.apache.commons.lang.StringUtils;
 import org.bstats.bungeecord.Metrics.SimplePie;
 
 public class LanguageData extends SimplePie {
@@ -17,13 +18,22 @@ public class LanguageData extends SimplePie {
     return isValidLanguage(configLanguage) ? configLanguage : "custom";
   }
 
+  /**
+   * Checks if a given language string is valid. Supports xx_yy and xx_yy_z, where xx is the
+   * language code, yy is the country code and z is any positive integer
+   *
+   * @param lang language string to check
+   * @return If the passed language string is correct
+   */
   @VisibleForTesting
   static boolean isValidLanguage(String lang) {
-    String[] parts = lang.split("_", 3);
+    String[] parts = lang.split("_", 4);
 
-    if (parts.length != 2) return false;
+    if (parts.length < 2 || parts.length > 3) return false;
 
     try {
+      if (parts.length == 3 && !StringUtils.isNumeric(parts[2])) return false;
+
       final Locale loc = new Locale(parts[0], parts[1]);
 
       return (loc.getISO3Language() != null) && (loc.getISO3Country() != null);
